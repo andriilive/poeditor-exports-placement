ad0b5b7c523646857f1f8131ca6a689c7177f078
Andrii Iv. (@digitalandyeu) <hi@digitalandy.eu>
2022-12-08 14:27:50 +0100

    poeditor json auto placer

---
+++ cp.sh
@@ -0,0 +1,34 @@
+source .env
+SOURCE_FOLDER=~/Downloads
+DESTINATION_FOLDER=~/IdeaProjects/$PROJECT-web/locales
+
+declare -A MAP_LANG
+MAP_LANG[Czech]="cs"
+MAP_LANG[English]="en"
+MAP_LANG[German]="de"
+MAP_LANG[Hungarian]="hu"
+MAP_LANG[Italian]="it"
+MAP_LANG[Polish]="pl"
+MAP_LANG[Serbian]="sr"
+MAP_LANG[Russian]="ru"
+MAP_LANG[French]="fr"
+
+LOG_INFO="SOURCE_FOLDER: $SOURCE_FOLDER | DESTINATION_FOLDER: $DESTINATION_FOLDER"
+LOG_FILES=()
+
+for file in $SOURCE_FOLDER/$PROJECT_*_*.json; do
+    filename=$file:t:r
+
+    ns=${filename%_*}
+    ns=${ns##*_}
+
+    cp_me=$file
+    cp_to=$DESTINATION_FOLDER/${MAP_LANG[${filename##*_}]}/$ns.json
+
+    LOG_FILES+=("[$(date +%Y-%m-%d-%H:%M:%S)] $cp_me -> $cp_to")
+    cp -f "$cp_me" "$cp_to"
+done
+
+for LOG_FILE in "${LOG_FILES[@]}"; do
+    echo $LOG_FILE >> debug.log
+done
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Poeditor json exports mapping
+
+[![StandWithUkraine](https://raw.githubusercontent.com/vshymanskyy/StandWithUkraine/main/badges/StandWithUkraine.svg)](https://github.com/vshymanskyy/StandWithUkraine/blob/main/docs/README.md)
+
+Maps [poeditor](https://poeditor.com/projects) json exports to your project translations folder
+
+
+```bash
+$ cp .env.example .env
+```
+
+[@yolkstudio](https://github.com/yolkstudio)
